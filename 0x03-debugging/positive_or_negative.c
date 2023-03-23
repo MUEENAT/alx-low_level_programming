0f43b9ba1d7550e7ffa84462742a4e33f2c2462e
MUEENAT <fasholamueenah@gmail.com>
2023-03-23 07:54:11 -0700

    comment out infinite loop

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,11 +1,21 @@
 #include "main.h"
+/**
+ * main - Entry point
+ * description: "positive_or_negative"
+ *Return: always 0
+ */
 
-void positive_or_negative(int i)
+int main(void)
 {
+	int i;
+
+	i = 0
+
 	if (i > 0)
 		printf("%d is positive\n", i);
 	else if (i < 0)
 		printf("%d is negative\n", i);
 	else
 		printf("%d is zero\n", i);
+	return (0);
 }
