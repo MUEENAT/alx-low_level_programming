a5a7426f50e2bbbddd4dad0a419c5344320ab223
MUEENAT <fasholamueenah@gmail.com>
2023-03-27 08:23:15 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_to_98 - function that takes a pointer to an int
+ * as paramter and updates the value it point to to 98
+ * @n: input
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
