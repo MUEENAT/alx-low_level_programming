74f34cb2d37edf606475233997e9637b2aa6433a
MUEENAT <fasholamueenah@gmail.com>
2023-03-29 05:12:51 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcat -  concatenates two strings
+ * @src: source string
+ * @dest: destination string
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int dlength = 0;
+	int slength = 0;
+	int n;
+
+	for (n = 0; dest[n] != '\0'; n++)
+		dlength++;
+	for (n = 0; src[n] != '\0'; n++)
+		slength++;
+	for (n = 0; n <= slength; n++)
+		dest[dlength + n] = src[n];
+	return (dest);
+}
