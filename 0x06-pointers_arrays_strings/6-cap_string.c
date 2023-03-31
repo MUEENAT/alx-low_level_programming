2cccd8c641ae011c9f4381caac672c811ccd173d
MUEENAT <fasholamueenah@gmail.com>
2023-03-31 07:05:33 -0700

    7. Mozart composed his music not for the elite, but for everybody

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * cap_string -  capitalizes all words of a string.
+ * @str: string parameter
+ * Return: str
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+			continue;
+	}
+	{
+		if (str[i] == (' ' || '!' || .)
+			i++;
+	}
+	{
+	 if (str[i] >= 'a' && str[i] <= 'z')
+	  	str[i] = str[i] - 32;
+		continue;
+	}
+	else
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		str[i] = str[i] + 32;
+	}
+	return (str);
+}
