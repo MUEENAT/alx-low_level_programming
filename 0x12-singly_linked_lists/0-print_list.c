e54f50b3903bce62e3ee3855970213ea294ee0b7
MUEENAT <fasholamueenah@gmail.com>
2023-04-30 04:47:58 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,16 +11,16 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (h-> str == NULL)
-		{	
+		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%d] %s\n",h-> len, h-> str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		node++;
-		h = h-> next;
+		h = h->next;
 	}
 	return (node);
 }
