621e68312fa18c7d34cd8f1841f59c0d52b63569
MUEENAT <fasholamueenah@gmail.com>
2023-06-09 08:15:38 -0700

    1 args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,9 +6,8 @@
  * Return: always 0
  */
 
-int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
-
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-        printf("%d\n", argc - 1);
-                return (0);
+	printf("%d\n", argc - 1);
+		return (0);
 }
