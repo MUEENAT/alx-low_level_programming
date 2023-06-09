1ff370122e37c772fd49739a791f42012ce9ddf2
MUEENAT <fasholamueenah@gmail.com>
2023-06-09 08:10:09 -0700

    1. args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - prints its name, followed by a new line
+ * @argc: argument count(unused)
+ * @argv: argument vector
+ * Return: always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
+
+{
+        printf("%d\n", argc - 1);
+                return (0);
+}
