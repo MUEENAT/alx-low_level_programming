0e4954a8aaa28d5e8e3456c03061cc6fe3e49394
MUEENAT <fasholamueenah@gmail.com>
2023-06-13 13:08:13 -0700

    3. If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 	{
-        	return (NULL);
+		return (NULL);
 	}
 
 	pointer = malloc(height * sizeof(int *));
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 	}
 
 	for (i = 0; i < height; i++)
-	{	
+	{
 		pointer[i] = malloc(width * sizeof(int));
 		if (pointer == NULL)
 		{
