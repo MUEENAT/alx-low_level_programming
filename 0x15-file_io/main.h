21645a106d790e6bfebd69ab35eb12a8dd075652
MUEENAT <Fasholamueenah@gmail.com>
2023-07-16 06:00:12 -0700

    elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,18 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <elf.h>
+
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
